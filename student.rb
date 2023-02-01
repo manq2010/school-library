be6ed5991e0a8745fc70e3275e897e9d69ed5247
manq2010 <manq2010@gmail.com>
2023-02-01 16:45:25 +0200

    Add 'classroom' and 'book' classes

--- student.rb
+++ student.rb
@@ -1,8 +1,15 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+    # @classroom = classroom
+    belongs_to(classroom)
+  end
+
+  def belongs_to(classroom)
     @classroom = classroom
   end
 
